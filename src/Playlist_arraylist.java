c967232abd31b1fac547a57dbebf8c478d078196
revanthreddy1906 <revanthreddy22326@gmail.com>
2026-06-01 16:33:30 +0530

    Add files via upload

---
+++ src/Playlist_arraylist.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Playlist_arraylist {
+    public static void main(String[] args) {
+        List<String> Playlist=new ArrayList<>();
+        Scanner sc=new Scanner(System.in);
+        String firstsong=sc.nextLine();
+        String secondsong=sc.nextLine();
+        String thirdsong=sc.nextLine();
+        String fourthsong=sc.nextLine();
+        String fifthsong=sc.nextLine();
+        Playlist.add(firstsong);
+        Playlist.add(secondsong);
+        Playlist.add(thirdsong);
+        Playlist.add(fourthsong);
+        Playlist.add(fifthsong);
+        System.out.println("Playlist: "+Playlist);
+        Playlist.remove(3);
+        System.out.println("After removing song: "+Playlist);
+        System.out.println("the song at index(1): "+Playlist.get(1));
+        for(String s:Playlist)
+        {
+            System.out.println(s+" ");
+        }
+    }
+}
